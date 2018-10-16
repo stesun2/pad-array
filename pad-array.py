78563b0849914166d03d262c2a46524f85161fb5
Jonathan Young <jonyoungg@gmail.com>
2018-10-16 15:37:18 -0500

    Update pad-array.py

--- pad-array.py
+++ pad-array.py
@@ -1,8 +1,3 @@
 #REMEMBER TO PSEUDOCODE
-
-def pad!(array, min_size, value = None): #destructive
-  # Your code here
-
-
 def pad(array, min_size, value = None): #non-destructive
   # Your code here

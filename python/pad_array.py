31bcbc551086264ff3d19bacc4c9287e3ad191c2
stesun2 <ss27@illinois.edu>
2021-05-31 20:02:08 -0500

    pad_array test outline

--- python/pad_array.py
+++ python/pad_array.py
@@ -1,2 +1,6 @@
 #REMEMBER TO PSEUDOCODE
 def pad(array, min_size, value = None):
+    return 0
+
+
+
--- python/pad_array_spec.py
+++ python/pad_array_spec.py
@@ -1,1 +1,26 @@
 # Write unit tests!
+import unittest
+from pad_array import pad_array
+
+class PadArrayTestCase(unittest.TestCase):
+    """Test for pad_array.py"""
+
+    def test_returns_empty_list(self):
+        """Calls pad_array.py, returns an empty list"""
+        self.assertEqual(len(pad([''],'', '')), [])
+    
+    def test_returns_min_size(self):
+        """Calls pad_array.py, returns the length of min size """
+        self.assertEqual()
+
+    def test_returns_padded_list(self):
+        """Calls pad_array.py, returns a padded list mode"""
+        self.assertEqual()
+
+    def test_returns_non_negative_int(self):
+        """Calls pad_array.py, checks if min_size is non-negative integer"""
+        self.assertEqual()
+
+
+if __name__ == '__main__':
+    unittest.main()
